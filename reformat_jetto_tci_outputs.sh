cb94e01b39d6fca7239b7e518c74a855b15bc802
Harry Dudding <harry.dudding@ukaea.uk>
2023-10-24 06:48:07 +0100

    condense output file writing

--- reformat_jetto_tci_outputs.sh
+++ reformat_jetto_tci_outputs.sh
@@ -17,23 +17,15 @@ do
 	# separate data, make files
 	head -n $(( $ANOM_HEADER+4+$NGRID-1 )) TCI_debug.dat | tail -n $(($NGRID-1)) | awk '{print $1}' > reformatted_jetto_tci/tci_grid
 	
-	head -n $(( $ANOM_HEADER+4+$NGRID-1 )) TCI_debug.dat | tail -n $(($NGRID-1)) > reformatted_jetto_tci/heat_fluxes
-	
-	head -n $(( $ANOM_HEADER+4+(2*$NGRID) )) TCI_debug.dat | tail -n $(($NGRID-1)) > reformatted_jetto_tci/particle_fluxes
-	
-	head -n $(( $ANOM_HEADER+4+(3*$NGRID) + 1 )) TCI_debug.dat | tail -n $(($NGRID-1)) > reformatted_jetto_tci/heat_diffusivities_diagonal
-	
-	head -n $(( $ANOM_HEADER+4+(4*$NGRID) + 2 )) TCI_debug.dat | tail -n $(($NGRID-1)) > reformatted_jetto_tci/particle_diffusivities_diagonal
-	
-	head -n $(( $ANOM_HEADER+4+(5*$NGRID) + 3 )) TCI_debug.dat | tail -n $(($NGRID-1)) > reformatted_jetto_tci/heat_pinch_velocities
-	
-	head -n $(( $ANOM_HEADER+4+(6*$NGRID) + 4 )) TCI_debug.dat | tail -n $(($NGRID-1)) > reformatted_jetto_tci/particle_pinch_velocities
-	
-	head -n $(( $ANOM_HEADER+4+(7*$NGRID) + 5 )) TCI_debug.dat | tail -n $(($NGRID-1)) > reformatted_jetto_tci/toroidal_mom_diff
-	
-	head -n $(( $ANOM_HEADER+4+(8*$NGRID) + 6 )) TCI_debug.dat | tail -n $(($NGRID-1)) > reformatted_jetto_tci/eff_heat_diffs
-	
-	head -n $(( $ANOM_HEADER+4+(9*$NGRID) + 7 )) TCI_debug.dat | tail -n $(($NGRID-1)) > reformatted_jetto_tci/eff_particle_diffs
+	i=1
+	for filename in heat_fluxes particle_fluxes heat_diffusivities_diagonal\
+	particle_diffusivities_diagonal heat_pinch_velocities particle_pinch_velocities\
+	toroidal_momentum_diffusivities effective_heat_diffusivities effective_particle_diffusivities
+	do
+		head_ind=$((($i*$NGRID)+$i-2))
+		head -n $(( $ANOM_HEADER+4+$head_ind )) TCI_debug.dat | tail -n $(($NGRID-1)) > reformatted_jetto_tci/$filename
+		i=$(($i+1))
+	done
 	
 	echo "Reformatted jetto tci outputs generated."
 	
