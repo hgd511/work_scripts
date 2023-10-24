05ffd270ed9c1adc0bbd6cb33d2328df2f7f2a40
Harry Dudding <harry.dudding@ukaea.uk>
2023-10-24 11:50:34 +0100

    reformatted electron and geometry tci quantities

--- reformat_jetto_tci_outputs.sh
+++ reformat_jetto_tci_outputs.sh
@@ -27,6 +27,26 @@ do
 		i=$(($i+1))
 	done
 	
+	# electron quantities
+	elec_header=$(grep -n " rho ne Te vtor_e vpol_e Zeff ns" TCI_debug.dat | cut -d : -f 1)
+	
+	head -n $(( $elec_header + $NGRID )) TCI_debug.dat | tail -n $NGRID > reformatted_jetto_tci/electron_quantities
+	
+	head -n $(( $elec_header + (2*$NGRID) + 1 )) TCI_debug.dat | tail -n $NGRID > reformatted_jetto_tci/wexb_alpha_psi_ps_e_rad
+	
+	# geometry quantities
+	geo_header=$(grep -n " a0,R0,Bt0, arho, Ip, BIC" TCI_debug.dat | cut -d : -f 1)
+	
+	head -n $(( $geo_header + 1 )) TCI_debug.dat | tail -n 1 > reformatted_jetto_tci/a0_R0_Bt0_arho_Ip_BIC
+	
+	i=1
+	for filename in rminor_rmajor_kappa_vprime q_shear_grho1_grho2\
+	b2_bm2_fhat_gr2bm2 fms ft_grth_psi_dpsidrho f_jdotBoverB0 delta_btor_bpol
+	do
+		head -n $(( $geo_header+2+($i*$NGRID)+$i-1 )) TCI_debug.dat | tail -n $NGRID > reformatted_jetto_tci/$filename
+		i=$(($i+1))
+	done
+	
 	echo "Reformatted jetto tci outputs generated."
 	
 done
